628a83e78366090d93a4a9c4ceb4a6b85ed43b99
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-28 21:58:39 +0700

    feat: logged user

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -11,6 +11,12 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    if (!user) {
+      loadUser();
+    }
+  }, [user]);
+
   const login = async ({ username, password }) => {
     try {
       setLoading(true);
@@ -20,15 +26,36 @@ export const AuthProvider = ({ children }) => {
       });
 
       if (res.data.success) {
+        loadUser();
         setIsAuthenticated(true);
         setLoading(false);
-        router.push('/')
+        router.push('/');
       }
     } catch (error) {
       setLoading(false);
       setError(error.response && (error.response.data.detail || error.response.data.error));
     }
   }
+
+  const loadUser = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post('/api/auth/user');
+
+      if (res.data.user) {
+        setIsAuthenticated(true);
+        setLoading(false);
+        setUser(res.data.user);
+        console.log(user)
+      }
+    } catch (error) {
+      setLoading(false);
+      setIsAuthenticated(false);
+      setUser(null);
+      setError(error.response && (error.response.data.detail || error.response.data.error));
+    }
+  }
+
 
   return (
     <AuthContext.Provider
---
+++ pages/api/auth/user.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from "axios";
+import cookie from 'cookie';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const cookies = cookie.parse(req.headers.cookie || '');
+    const access = cookies.access || false;
+
+    if (!access) {
+      return res.status(401).json({
+        error: 'Login first to load user'
+      });
+    }
+
+    try {
+      const response = await axios.get(`${process.env.API_URL}/api/me/`,{
+        headers: {
+          'Authorization': `Bearer ${access}`
+        }
+      });
+
+      if (response.data) {
+        return res.status(200).json({
+          user: response.data
+        })
+      }
+    } catch (error) {
+      res.status(error?.response.status).json({
+        error: 'Something went wrong while retrieving user'
+      });
+    }
+  }
+}
