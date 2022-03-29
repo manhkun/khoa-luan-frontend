7086407472f4d17a752ff821fab4eb70bf07abe1
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-29 21:28:32 +0700

    feat: current user profile

---
+++ pages/me/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/layouts";
+import UpdateProfile from "../../components/user/UpdateProfile";
+
+
+export default function UpdateProfilePage() {
+  return (
+    <Layout title="Update profile">
+      <UpdateProfile />
+    </Layout>
+  )
+}
